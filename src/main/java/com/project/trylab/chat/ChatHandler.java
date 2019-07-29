ae80f86aba9df7d2aeea951dc303787b326b3865
Cameobc <dkqtk@naver.com>
2019-07-29 14:48:01 +0900

    설정

---
+++ src/main/java/com/project/trylab/chat/ChatHandler.java
@@ -0,0 +1,46 @@
+package com.project.trylab.chat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class ChatHandler extends TextWebSocketHandler {
+
+	
+	//seeeion 아이디를 담을 후보군
+	//1. list    2.ConcurrentHashMap
+	private List<WebSocketSession> list = new ArrayList<WebSocketSession>();
+	
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		// TODO Auto-generated method stub
+		super.afterConnectionEstablished(session);
+	}
+
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		// TODO Auto-generated method stub
+		super.handleTextMessage(session, message);
+	}
+
+	@Override
+	public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+		// TODO Auto-generated method stub
+		super.handleTransportError(session, exception);
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		// TODO Auto-generated method stub
+		super.afterConnectionClosed(session, status);
+	}
+
+
+	
+	
+}
