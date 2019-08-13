9bcfaf74ec119e0308ac68248b606450c0c7b0a7
cameobc <48780780+Cameobc@users.noreply.github.com>
2019-08-13 14:42:24 +0900

    test

--- src/main/java/com/project/mail/MailUtils.java
+++ src/main/java/com/project/mail/MailUtils.java
@@ -8,12 +8,19 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 
 public class MailUtils {
 
+	//
 	private JavaMailSender mailSender;
 	private MimeMessage message;
+	//messagehelper : subject 제목, to 수신자, from 발신자, test 내용. html 여부 설정 가능
 	private MimeMessageHelper messageHelper;
 	
 	public MailUtils(JavaMailSender mailSender) throws MessagingException {
-		
+		this.mailSender = mailSender;
+		message = this.message;
+		messageHelper = new MimeMessageHelper(message, true, "UTF-8");
 	}
 	
+	public void setSubject(String subject) throws MessagingException{
+		messageHelper.setSubject(subject);
+	}
 }
